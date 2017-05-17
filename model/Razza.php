a7f3b36712e12b2d0fd7b340cf7c5e43b084a133
MattiCerio <matteo.cerioni@live.it>
2017-05-17 09:44:07 +0200

    Create Razza.php

---
+++ model/Razza.php
@@ -0,0 +1,39 @@
+<?php
+class Razza{
+    private $id;
+    private $nome;
+    private $descrizione;
+    private $id_specie;
+    
+    function getID(){
+        return $this->id;
+    }
+    
+    function getNome(){
+        return $this->nome;
+    }
+    
+    function getDescrizione(){
+        return $this->descrizione;
+    }
+    
+    function getIDSpecie(){
+        return $this->id_specie;
+    }
+    
+    function setID($v){
+        $this->id=$v;
+    }
+    
+    function setNome($v){
+        $this->nome=$v;
+    }
+    
+     function setDescrizione($v){
+        $this->descrizione=$v;
+    }
+    
+     function setIDSpecie($v){
+        $this->id_specie=$v;
+    }
+}
