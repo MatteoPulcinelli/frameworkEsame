20593057b93306b248c28845e028eeb0aea3fcd4
MattiCerio <matteo.cerioni@live.it>
2017-05-17 09:40:48 +0200

    Create Annuncio.php

---
+++ model/Annuncio.php
@@ -0,0 +1,71 @@
+<?php 
+class Annuncio{
+	private $id;
+	private $titolo;
+    private $descrizione;
+    private $visualizzazioni;
+    private $id_utente;
+    private $id_provincia;
+    private $id_razza;
+	
+	function getID(){
+		return $this->id;
+	}
+	
+    function getTitolo(){
+        return $this->titolo;    
+    }
+    
+    function getDescrizione(){
+        return $this->descrizione;    
+    }
+    
+    function getVisualizzazioni(){
+        return $this->visualizzazioni
+    }
+    
+    function getIDUtente(){
+        return $this->id_utente;    
+    }
+    
+    function getIDProvincia(){
+        return $this->id_provincia;
+    }
+    
+    function getIDRazza(){
+        return $this->id_razza;
+    }
+    
+	function setID($v){
+		$this->id=$v;
+	}
+	
+	function setTitolo($v){
+		$this->titolo=$v;
+	}
+	
+	function setDescrizione($v){
+		$this->descrizione=$v;
+	}
+	
+	function setVisualizzazioni($v){
+		$this->visualizzazioni=$v;
+	}
+	
+	function setIDUtente($v){
+		$this->id_utente=$v;
+	}
+	
+	function setIDProvincia($v){
+		$this->id_provincia=$v;
+	}
+	
+	function setIDRazza($v){
+		$this->razza=$v;
+	}
+	
+	function save(){
+		TabellaAnnunci::save($this);
+	}
+}
+?>
